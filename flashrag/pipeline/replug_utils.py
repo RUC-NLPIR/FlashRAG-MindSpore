5fe82581229a64d51d3531e7357b15e8c3ac7c67
Zhao Yang <131931159+lihai-zhao@users.noreply.github.com>
2024-09-24 16:14:03 +0800

    Add files via upload

--- flashrag/pipeline/replug_utils.py
+++ flashrag/pipeline/replug_utils.py
@@ -3,8 +3,8 @@
 
 import warnings
 from typing import List, Optional, Union
-import transformers
-from transformers import (
+
+from mindnlp.transformers import (
     MODEL_FOR_CAUSAL_LM_MAPPING,
     AutoConfig,
     GenerationMixin,
@@ -12,23 +12,27 @@ from transformers import (
     LogitsProcessorList,
     StoppingCriteriaList,
 )
-import torch
-import torch.nn as nn
-import torch.distributed as dist
-from transformers.generation.stopping_criteria import validate_stopping_criteria
-from transformers.generation.utils import (
+
+import mindspore as ms
+import mindspore.ops as ops
+
+from mindnlp.transformers.generation.stopping_criteria import validate_stopping_criteria
+from mindnlp.transformers.generation.utils import (
     SampleDecoderOnlyOutput,
     SampleEncoderDecoderOutput,
     SampleOutput,
 )
+import mindnlp.transformers as transformers_
+
 
 
 class REPLUG_Generation(GenerationMixin):
-    """Implementing REPLUG-based sampling text generation."""
+    """Implementing REPLUG-based sampling text generation.
+    """
 
     def sample(
         self,
-        input_ids: torch.LongTensor,
+        input_ids: ms.Tensor,
         logits_processor: Optional[LogitsProcessorList] = None,
         stopping_criteria: Optional[StoppingCriteriaList] = None,
         logits_warper: Optional[LogitsProcessorList] = None,
@@ -42,10 +46,14 @@ class REPLUG_Generation(GenerationMixin):
         synced_gpus: bool = False,
         streamer: Optional["BaseStreamer"] = None,
         **model_kwargs,
-    ) -> Union[SampleOutput, torch.LongTensor]:
+    ) -> Union[SampleOutput, ms.Tensor]:
         # init values
-        logits_processor = logits_processor if logits_processor is not None else LogitsProcessorList()
-        stopping_criteria = stopping_criteria if stopping_criteria is not None else StoppingCriteriaList()
+        logits_processor = (
+            logits_processor if logits_processor is not None else LogitsProcessorList()
+        )
+        stopping_criteria = (
+            stopping_criteria if stopping_criteria is not None else StoppingCriteriaList()
+        )
         if max_length is not None:
             warnings.warn(
                 "`max_length` is deprecated in this function, use"
@@ -54,17 +62,29 @@ class REPLUG_Generation(GenerationMixin):
             )
             stopping_criteria = validate_stopping_criteria(stopping_criteria, max_length)
         logits_warper = logits_warper if logits_warper is not None else LogitsProcessorList()
-        pad_token_id = pad_token_id if pad_token_id is not None else self.generation_config.pad_token_id
-        eos_token_id = eos_token_id if eos_token_id is not None else self.generation_config.eos_token_id
+        pad_token_id = (
+            pad_token_id if pad_token_id is not None else self.generation_config.pad_token_id
+        )
+        eos_token_id = (
+            eos_token_id if eos_token_id is not None else self.generation_config.eos_token_id
+        )
         if isinstance(eos_token_id, int):
             eos_token_id = [eos_token_id]
-        eos_token_id_tensor = torch.tensor(eos_token_id).to(input_ids.device) if eos_token_id is not None else None
-        output_scores = output_scores if output_scores is not None else self.generation_config.output_scores
+        eos_token_id_tensor = (
+            ms.tensor(eos_token_id) if eos_token_id is not None else None
+        )
+        output_scores = (
+            output_scores if output_scores is not None else self.generation_config.output_scores
+        )
         output_attentions = (
-            output_attentions if output_attentions is not None else self.generation_config.output_attentions
+            output_attentions
+            if output_attentions is not None
+            else self.generation_config.output_attentions
         )
         output_hidden_states = (
-            output_hidden_states if output_hidden_states is not None else self.generation_config.output_hidden_states
+            output_hidden_states
+            if output_hidden_states is not None
+            else self.generation_config.output_hidden_states
         )
         return_dict_in_generate = (
             return_dict_in_generate
@@ -80,13 +100,17 @@ class REPLUG_Generation(GenerationMixin):
 
         # if model is an encoder-decoder, retrieve encoder attention weights and hidden states
         if return_dict_in_generate and self.config.is_encoder_decoder:
-            encoder_attentions = model_kwargs["encoder_outputs"].get("attentions") if output_attentions else None
+            encoder_attentions = (
+                model_kwargs["encoder_outputs"].get("attentions") if output_attentions else None
+            )
             encoder_hidden_states = (
-                model_kwargs["encoder_outputs"].get("hidden_states") if output_hidden_states else None
+                model_kwargs["encoder_outputs"].get("hidden_states")
+                if output_hidden_states
+                else None
             )
 
         # keep track of which sequences are already finished
-        unfinished_sequences = torch.ones(input_ids.shape[0], dtype=torch.long, device=input_ids.device)
+        unfinished_sequences = ops.ones(input_ids.shape[0], dtype=ms.Int)
 
         this_peer_finished = False  # used by synced_gpus only
         # auto-regressive generation
@@ -94,9 +118,11 @@ class REPLUG_Generation(GenerationMixin):
             if synced_gpus:
                 # Under synced_gpus the `forward` call must continue until all gpus complete their sequence.
                 # The following logic allows an early break if all peers finished generating their sequence
-                this_peer_finished_flag = torch.tensor(0.0 if this_peer_finished else 1.0).to(input_ids.device)
+                this_peer_finished_flag = ms.tensor(0.0 if this_peer_finished else 1.0)
                 # send 0.0 if we finished, 1.0 otherwise
-                dist.all_reduce(this_peer_finished_flag, op=dist.ReduceOp.SUM)
+                # dist.all_reduce(this_peer_finished_flag, op=dist.ReduceOp.SUM)
+                all_reduce_sum = ops.AllReduce(ops.ReduceOp.SUM)
+                this_peer_finished_flag = all_reduce_sum(this_peer_finished_flag)
                 # did all peers finish? the reduced sum will be 0.0 then
                 if this_peer_finished_flag.item() == 0.0:
                     break
@@ -128,31 +154,39 @@ class REPLUG_Generation(GenerationMixin):
                     scores += (next_token_scores,)
                 if output_attentions:
                     decoder_attentions += (
-                        (outputs.decoder_attentions,) if self.config.is_encoder_decoder else (outputs.attentions,)
+                        (outputs.decoder_attentions,)
+                        if self.config.is_encoder_decoder
+                        else (outputs.attentions,)
                     )
                     if self.config.is_encoder_decoder:
                         cross_attentions += (outputs.cross_attentions,)
 
                 if output_hidden_states:
                     decoder_hidden_states += (
-                        (outputs.decoder_hidden_states,) if self.config.is_encoder_decoder else (outputs.hidden_states,)
+                        (outputs.decoder_hidden_states,)
+                        if self.config.is_encoder_decoder
+                        else (outputs.hidden_states,)
                     )
 
             ### REPLUG
             # Sample from the normalized "logits", assuming the REPLUG processor was used!
-            probs = nn.functional.softmax(next_token_scores, dim=-1)
-            next_tokens = torch.multinomial(probs, num_samples=1).squeeze(-1)
+            probs = ops.softmax(next_token_scores, axis=-1)
+            next_tokens = ops.multinomial(probs,2,False).squeeze(-1)
             # Lock same next-token for all examples in batch
             next_tokens[:] = next_tokens[0]
 
             # finished sentences should have their next token be a padding token
             if eos_token_id is not None:
                 if pad_token_id is None:
-                    raise ValueError("If `eos_token_id` is defined, make sure that `pad_token_id` is defined.")
-                next_tokens = next_tokens * unfinished_sequences + pad_token_id * (1 - unfinished_sequences)
+                    raise ValueError(
+                        "If `eos_token_id` is defined, make sure that `pad_token_id` is defined."
+                    )
+                next_tokens = next_tokens * unfinished_sequences + pad_token_id * (
+                    1 - unfinished_sequences
+                )
 
             # update generated ids, model inputs, and length for next step
-            input_ids = torch.cat([input_ids, next_tokens[:, None]], dim=-1)
+            input_ids = ops.cat([input_ids, next_tokens[:, None]], axis=-1)
             if streamer is not None:
                 streamer.put(next_tokens.cpu())
             model_kwargs = self._update_model_kwargs_for_generation(
@@ -162,7 +196,9 @@ class REPLUG_Generation(GenerationMixin):
             # if eos_token was found in one sentence, set sentence to finished
             if eos_token_id_tensor is not None:
                 unfinished_sequences = unfinished_sequences.mul(
-                    next_tokens.tile(eos_token_id_tensor.shape[0], 1).ne(eos_token_id_tensor.unsqueeze(1)).prod(dim=0)
+                    next_tokens.tile(eos_token_id_tensor.shape[0], 1)
+                    .ne(eos_token_id_tensor.unsqueeze(1))
+                    .prod(axis=0)
                 )
 
                 # stop when each sentence is finished
@@ -207,43 +243,43 @@ class REPLUGLogitsProcessor(LogitsProcessor):
 
     Reference: fastRAG
     """
-
-    def __init__(self, doc_scores: torch.FloatTensor):
+    def __init__(self, doc_scores: ms.Tensor):
         self.num_docs = doc_scores.shape[0]
         # normalize
         doc_scores /= doc_scores.sum()
-        self.doc_scores = torch.unsqueeze(doc_scores, 1)  # k*1
+        self.doc_scores = ops.unsqueeze(doc_scores, 1) # k*1
 
     def __call__(self, input_ids, scores):
         # doc_score: k*1, scores: k*vocab_size
         replug_scores = self.doc_scores * scores
-        replug_scores = replug_scores.sum(dim=0)  # 1*vocab_size
-        replug_scores = torch.tile(replug_scores, (self.num_docs, 1))  # k*vocab_size
+        replug_scores = replug_scores.sum(axis=0) # 1*vocab_size
+        replug_scores = ops.tile(replug_scores, (self.num_docs, 1)) # k*vocab_size
         return replug_scores
 
-
 def load_replug_model(name_or_path):
-    class HF_REPLUG:
-        "Creates a HF model that inherits from REPLUG_Generation class"
+        class HF_REPLUG:
+            "Creates a HF model that inherits from REPLUG_Generation class"
 
-        def __new__(cls, name_or_path, **kwargs):
-            return factory(name_or_path).from_pretrained(name_or_path, **kwargs)
+            def __new__(cls, name_or_path, **kwargs):
+                return factory(name_or_path).from_pretrained(name_or_path, **kwargs)
 
-    def factory(name_or_path):
-        loadedConfig = AutoConfig.from_pretrained(name_or_path)
-        try:
-            pretrained_class_object = getattr(transformers, loadedConfig.architectures[0])
-            if pretrained_class_object not in MODEL_FOR_CAUSAL_LM_MAPPING.values():
-                raise ValueError(f"Model {pretrained_class_object} is not used for causal LM generation.")
-        except AttributeError:
-            raise ValueError("Transformers architecture unknown.")
 
-        class HF(pretrained_class_object, REPLUG_Generation):
-            """Wrapper around HuggingFace transformers with REPLUG generation."""
+        def factory(name_or_path):
+            loadedConfig = AutoConfig.from_pretrained(name_or_path)
+            try:
+                pretrained_class_object = getattr(transformers_, loadedConfig.architectures[0])
+                if pretrained_class_object not in MODEL_FOR_CAUSAL_LM_MAPPING.values():
+                    raise ValueError(
+                        f"Model {pretrained_class_object} is not used for causal LM generation."
+                    )
+            except AttributeError:
+                raise ValueError("Transformers architecture unknown.")
 
-            _keys_to_ignore_on_load_unexpected = [r"cls"]
-            _tied_weights_keys = ["lm_head.weight"]
+            class HF(pretrained_class_object, REPLUG_Generation):
+                """Wrapper around HuggingFace transformers with REPLUG generation."""
 
-        return HF
+                _keys_to_ignore_on_load_unexpected = [r"cls"]
+                _tied_weights_keys = ["lm_head.weight"]
 
-    return HF_REPLUG(name_or_path)
+            return HF
+        return HF_REPLUG(name_or_path)
